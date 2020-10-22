0532659dfca838f6e1a64d5c10284e4a0a0b7f7c
Chetan Kini <ckini123@gmail.com>
2020-10-21 20:26:28 -0400

    Init commit

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,17 +2,17 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Chetan | Fullstack Developer', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello, my name is',
+  name: 'Chetan',
+  subtitle: 'Fullstack Developer | OSS Contributor',
+  cta: 'Explore',
 };
 
 // ABOUT DATA
